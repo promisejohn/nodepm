6998b01d91847759d1db8c7702e7d6dd238968fe
Nick Will <nickwill@Nicks-MacBook-Pro.local>
2018-03-12 23:15:33 +0800

    add server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const port = 3000;
+const server = http.createServer((req,res) => {
+    console.log(res.url);
+    res.end('Hello World!');
+});
+
+server.listen(port, (err)=>{
+    if(err) { 
+       console.log('sth went wrong', err);
+       return false;
+    }
+    console.log('Server is running on port ${port}');
+});
