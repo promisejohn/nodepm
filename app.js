c067841a7abd0a67b9556c579867fa236f9b428b
promisejohn <promise.john@gmail.com>
2018-03-15 23:44:24 +0800

    modify app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = 3000;
 const server = http.createServer((req,res) => {
     console.log(res.url);
-    res.end('Hello World!');
+    res.end('Hello World from app.js!');
 });
 
 server.listen(port, (err)=>{
